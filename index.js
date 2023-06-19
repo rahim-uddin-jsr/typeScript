1e6ac5216edba14c61f17657046e2683d723249f
dev-rahim <rahimuddin5915@gmail.com>
2023-06-20 01:06:03 +0600

    first typeScript code run done!

---
+++ index.js
@@ -0,0 +1,7 @@
+var userName = "Rahim Uddin";
+console.log(userName);
+var Add = function (a, b) {
+    return a + b;
+};
+console.log(Add(10, 20));
+// console.log(Add(10,'10'));
---
+++ index.ts
@@ -0,0 +1,9 @@
+let userName: string = "Rahim Uddin"
+console.log(userName);
+
+const Add = (a: number, b: number): number => {
+    return a + b
+}
+
+console.log(Add(10, 20));
+// console.log(Add(10,'10'));
